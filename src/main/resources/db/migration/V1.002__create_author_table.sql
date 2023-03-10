65c92cfdd84767f0ce5c87a8d7b61d7fcf0c5fdb
Jakub <jchyla@softsystem.pl>
2023-03-10 08:40:16 +0100

    clear-working-data-base

--- src/main/resources/db/migration/V1.002__create_author_table.sql
+++ src/main/resources/db/migration/V1.002__create_author_table.sql
@@ -26,4 +26,8 @@ INSERT INTO book_author (book_id, author_id)
 VALUES (1, 1),
        (1, 2),
        (2, 1),
-       (2, 2);
+       (2, 2),
+       (3, 3),
+       (3, 4),
+       (4, 4),
+       (4, 1);
--- src/main/resources/db/migration/V1.003__create_signature_table.sql
+++ src/main/resources/db/migration/V1.003__create_signature_table.sql
@@ -12,4 +12,8 @@ INSERT INTO signature (id, book_id, book_signature)
 VALUES (1, 4, 'yyy'),
        (2, 3, 'xxx'),
        (3, 2, 'eee'),
-       (4, 1, 'hhh');
+       (4, 1, 'hhh'),
+       (5, 4, 'yyy1'),
+       (6, 3, 'xxx1'),
+       (7, 2, 'eee1'),
+       (8, 1, 'hhh1');
