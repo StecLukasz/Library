b64286d59bdb46fe29ff8f749c4ba535945a2340
Jakub <jchyla@softsystem.pl>
2023-03-15 12:28:09 +0100

    #1-Zakładka1- reparing flyway;

--- src/main/resources/db/migration/V1.004__create_borrowed_table.sql
+++ src/main/resources/db/migration/V1.004__create_borrowed_table.sql
@@ -6,13 +6,13 @@ CREATE TABLE borrowed
     borrowed_date DATE         NOT NULL,
     overdue_date  DATE NULL,
     return_date   DATE         NOT NULL,
-    status_data   date NULL,
+    status_date   date NULL,
     status        VARCHAR(255) NOT NULL,
 
     PRIMARY KEY (id),
     FOREIGN KEY (signature_id) REFERENCES signature (id)
 );
-INSERT INTO borrowed (id, login, signature_id, borrowed_date, overdue_date, return_date,status_data, status)
+INSERT INTO borrowed (id, login, signature_id, borrowed_date, overdue_date, return_date, status_date, status)
 VALUES (1, 'dmichna', 1, '01-02-2000', '01-02-2000', '01-02-2000', '01-02-2000', 'borrowed'),
        (2, 'dmichna', 1, '05-06-1966', '01-02-2000', '01-02-2000', '01-02-2000', 'returned'),
        (3, 'lstec', 2, '09-10-1995', '01-02-2000', '01-02-2000', '01-02-2000', 'borrowed'),
