6953add3bb7478ce65eae7d2cf854b797195728f
Jakub <jchyla@softsystem.pl>
2023-04-25 09:21:19 +0200

    #merge-demo-vol2 - remove book with a couple signatures

---
+++ src/main/java/pl/softsystem/books/domain/BookAuthorRepository.java
@@ -0,0 +1,15 @@
+package pl.softsystem.books.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface BookAuthorRepository extends JpaRepository<BookAuthor, Long> {
+
+     BookAuthor findByBookId(Long bookId);
+     @Transactional
+     void deleteByBookId(Long bookId);
+}
